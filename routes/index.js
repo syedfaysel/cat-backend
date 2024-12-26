499ed43df6a87c9b236f440e16daf66ae6a033b7
Syed Faysel Ahammad Rajo <sfa.rajo20@gmail.com>
2024-12-26 15:38:03 +0600

    catPost controllers, routes added [Zubaida]

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ import authMiddleware from "../middlewares/authMiddleware.js";
 import testRoutes from "./testRoutes.js";
 import authRoutes from "./authRoutes.js";
 import catRoutes from "./catRoutes.js"
+import catPostRoutes from "./catPostRoutes.js";
 
 
 const router = express.Router();
@@ -13,5 +14,6 @@ const path = "/api/v1/";
 router.use(`${path}test`, authMiddleware, testRoutes); //api/v1/test/test-get
 router.use(`${path}auth`, authRoutes);
 router.use(`${path}cats`, catRoutes);
+router.use(`${path}cat-posts`, catPostRoutes);
 
 export default router;
